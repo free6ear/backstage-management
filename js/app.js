e6d9f948f3d0fc38ac9b54028eaa2fc9ae32afd5
sunmeng <free6ear@gmail.com>
2019-07-19 12:40:49 +0800

    7/19

--- js/app.js
+++ js/app.js
@@ -8,19 +8,15 @@ function initFileInput(ctrlName) {
         language: 'zh', //设置语言
         uploadUrl: "", //上传的地址
         allowedFileExtensions: ['pdf'],//接收的文件后缀
-        maxFilesNum : 5,//上传最大的文件数量
+        maxFilesNum : 10,//上传最大的文件数量
         //uploadExtraData:{"id": 1, "fileName":'123.mp3'},
         uploadAsync: true, //默认异步上传
         showUpload: true, //是否显示上传按钮
         showRemove : true, //显示移除按钮
         showPreview : true, //是否显示预览
         showCaption: false,//是否显示标题
-        browseClass: "btn btn-primary", //按钮样式
+        browseClass: "btn btn-danger", //按钮样式
         //dropZoneEnabled: true,//是否显示拖拽区域
-        //minImageWidth: 50, //图片的最小宽度
-        //minImageHeight: 50,//图片的最小高度
-        //maxImageWidth: 1000,//图片的最大宽度
-        //maxImageHeight: 1000,//图片的最大高度
         maxFileSize: 0,//单位为kb，如果为0表示不限制文件大小
         //minFileCount: 0,
         //maxFileCount: 10, //表示允许同时上传的最大文件个数
